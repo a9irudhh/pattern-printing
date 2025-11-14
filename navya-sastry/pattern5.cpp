0d7048238803530a26fdc508e521a4602f088389
navyasastry06 <navyamogh@gmail.com>
2025-11-14 12:26:20 +0530

    added two more patterns

--- navya-sastry/pattern5.cpp
+++ navya-sastry/pattern5.cpp
@@ -4,17 +4,20 @@ using namespace std;
 int main() {
 	// your code goes here
 	
-	int i,j,n;
-	cout<<"Give ur input: ";
+	int i,n;
+	char j;
 	cin>>n;
-	for(i=0;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
-	    for(j=0;j<=i;j++)
+	    for(j=1;j<=n;j++)
 	    {
+	        if(i==1||i==n||j==1||j==n)
 	        cout<<"*";
+	        else
+	        cout<<" ";
 	    }
 	    
 	    cout<<endl;
 	}
-   return 0;
+
 }
