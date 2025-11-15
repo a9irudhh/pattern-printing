e2d5e5926db53554b462c45901379bef6753650b
Sanskruti Agasgekar <01fe24bcs169@kletech.ac.in>
2025-11-15 21:09:57 +0530

    Add files via upload

---
+++ Sanskruti-316/all (7).cpp
@@ -0,0 +1,42 @@
+ /*pattern printing 7
+ 
+7. Inverted Pyramid
+Input: 5 Output:
+
+*********
+ *******
+  *****
+   ***
+    *
+
+
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    n=5;
+  
+
+   for( int i=0;i<n;i++)
+   {
+       for(int j=0;j<(i);j++)
+       {
+           cout<<" ";
+       }
+        for(int j=0;j<2*(n-i)-1;j++)
+       {
+           cout<<"*";
+       }
+     
+       cout<<endl;
+   }
+    
+  
+   
+    return 0;
+}
