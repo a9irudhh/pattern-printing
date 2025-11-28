658be325e9ea797c088c620e835a0ee553f4e1bd
Sanskruti Agasgekar <01fe24bcs169@kletech.ac.in>
2025-11-28 20:29:32 +0530

    Refactor Zig-Zag Star  changed the code
    Updated the Zig-Zag Star Pattern program to take user input for the number of rows and adjusted the logic accordingly.

--- Sanskruti-316/pattern13.cpp
+++ Sanskruti-316/pattern13.cpp
@@ -17,30 +17,20 @@ Input: 3 Output:
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() 
+{
     int n;
-     n=3;
-
-    for(int i = 0; i < n; i++) {
-       cout<<"*";
-        for(int j = 0; j < i; j++)
-        cout << " ";
-
-        cout << "*";
-
-       
-        if(i != n - 1) {
-            for(int j = 0; j < 2*(n - i - 1) - 1; j++)
-            cout << " ";
-            cout << "*";
+    cin >> n;             
+    int c=4*n-3;     
+    for (int i=1; i<=3; i++)
+     {
+      for (int j=1; j<=c; j++)
+         {
+            if ((i+j)%4==0 || (i==2 && j%4==0))
+                cout << "*";
+            else
+                cout << " ";
         }
-         for(int j = 0; j < i; j++)
-        cout << " ";
-
-   cout<<"*";
-        cout << "\n";
+        cout << endl;
     }
-
-    return 0;
 }
-
